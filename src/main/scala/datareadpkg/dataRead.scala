2c7f215b5145d428d0ff61f016b4be473ff54922
Sandeeplal <lalmsandp@gmail.com>
2018-07-22 01:26:40 +0530

    Added dataframe functions

--- src/main/scala/datareadpkg/dataRead.scala
+++ src/main/scala/datareadpkg/dataRead.scala
@@ -2,26 +2,32 @@ package datareadpkg
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
 object dataRead {
 
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setMaster("local[*]").setAppName("SparkMe App")
-    //  val spark = new SparkSession.Builder().master("local[*]").appName("SparkMe App")
     val sc = new SparkContext(conf)
-    val sqlc = new org.apache.spark.sql.hive.HiveContext(sc)
+    val sqlc = new SparkSession.Builder().enableHiveSupport().config(conf).getOrCreate()
+   // sc.setLogLevel("ERROR")
     import sqlc.implicits._
     val path = "D:\\Hadoop\\Projects\\Project for submission\\Project 1\\Project 1_dataset_bank-full.csv"
-    //    val basedb = spark.read.format("csv").option("delimiter", ";").option("header", "true").option("inferSchema", "true")
-    //      .load(path)
-    //    basedb.printSchema()
-
     val db1 = sc.textFile(path)
     val db = db1.filter(l => !(l.startsWith("\"age;")))
     val formatted_data = db.map(s => s.replace("\"", "")).map(s => s.split(";"))
     val val_df1 = formatted_data.map(p => Table(p(0).toInt, p(1), p(2), p(3), p(4), p(5).toInt, p(6), p(7), p(8), p(9).toInt, p(10), p(11).toInt, p(12).toInt, p(13).toInt, p(14).toInt, p(15), p(16))).toDF()
     val s = val_df1.createOrReplaceTempView("Customer")
-    sqlc.sql("Select (Select count(y) from Customer where y='yes')/count(*) from Customer").show()
+    val success_rate = sqlc.sql("Select (Select count(y) from Customer where y='yes')/count(*) from Customer")
+    println("Sucess_rate" + success_rate.collectAsList().get(0).getDouble(0))
+    val failure_rate = sqlc.sql("Select (Select count(y) from Customer where y='no')/count(*) from Customer")
+    println("failure_rate" + failure_rate.collectAsList().get(0).getDouble(0))
+    /// Using data frame methods calcaulat min, max and average; imported sql.functions
+
+    val avg_age = val_df1.select(mean("age"), max("age"), min("age")).collectAsList()
+    println("Average age= " + avg_age.get(0).get(0) + "\n max age = " + avg_age.get(0).get(1) + "\n min age = " + avg_age.get(0).get(2))
 
   }
 }
