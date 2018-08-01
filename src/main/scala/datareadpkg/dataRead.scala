75ca6a97b3b8c28e65118ca2f58b3adf681d06c0
Sandeeplal <lalmsandp@gmail.com>
2018-08-01 21:52:21 +0530

    Added Spark SQL Query

--- src/main/scala/datareadpkg/dataRead.scala
+++ src/main/scala/datareadpkg/dataRead.scala
@@ -3,6 +3,7 @@ package datareadpkg
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
+
 object dataRead {
 
   def main(args: Array[String]): Unit = {
@@ -15,6 +16,7 @@ object dataRead {
     val db = db1.filter(l => !l.startsWith("\"age;"))
     val formatted_data = db.map(s => s.replace("\"", "")).map(s => s.split(";"))
     val val_df1 = formatted_data.map(p => Table(p(0).toInt, p(1), p(2), p(3), p(4), p(5).toInt, p(6), p(7), p(8), p(9).toInt, p(10), p(11).toInt, p(12).toInt, p(13).toInt, p(14).toInt, p(15), p(16))).toDF()
+    val_df1.persist()
     val_df1.createOrReplaceTempView("Customer")
     val success_rate = sqlc.sql("Select (Select count(y) from Customer where y='yes')/count(*) from Customer")
     println("Sucess_rate = " + success_rate.collectAsList().get(0).getDouble(0))
@@ -24,6 +26,11 @@ object dataRead {
 
     val avg_age = val_df1.select(mean("age"), max("age"), min("age")).collectAsList()
     println("Average age= " + avg_age.get(0).get(0) + "\nmax age = " + avg_age.get(0).get(1) + "\nmin age = " + avg_age.get(0).get(2))
+
+    val bal = val_df1.select(avg("balance")).collectAsList()
+    println("Average of Balance amount in account is" + bal.get(0).get(0))
+    val age_in_sub = sqlc.sql("Select age, count(age) as No_of_people from(Select age from Customer where y='yes') Group By age Order By No_of_people desc")
+    age_in_sub.show()
 
   }
 }
